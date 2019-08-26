48f0f7d90d413c2b2cb1bcfdd64c0cf7eb203f44
superTO <to840116@gmail.com>
2019-08-27 00:17:11 +0800

    use timer update time

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import * as moment from 'moment';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, timer } from 'rxjs';
 
 
 @Component({
@@ -11,16 +11,18 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class MainComponent implements OnInit {
    public NowTime = new BehaviorSubject<string>(moment().format('HH:mm:ss'));
+   public timer = timer(1000, 1000);
   // public NowTime = new Date();
 
   public break: number;
   public session: number;
 
   constructor(public ref: ChangeDetectorRef) {
-    setInterval(() => {
-      this.NowTime.next(moment().format('HH:mm:ss'));
-    }, 5000);
+    // setInterval(() => {
+    //   this.NowTime.next(moment().format('HH:mm:ss'));
+    // }, 500);
     // this.ref.detectChanges();
+    this.timer.subscribe(() => this.NowTime.next(moment().format('HH:mm:ss')));
 
     this.break = 0;
     this.session = 0;
